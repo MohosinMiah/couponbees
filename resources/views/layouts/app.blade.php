1bfea2abf7940de8ba98b2fc9e98da246eff1e12
MohosinMiah <hamza1610330816@gmail.com>
2026-08-07 00:43:28 +0600

    Google ads

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -45,6 +45,17 @@
     fbq('track', 'PageView');
     </script>
     {{-- End Meta Pixel Code --}}
+
+    {{-- Google tag (gtag.js) --}}
+    <script async src="https://www.googletagmanager.com/gtag/js?id=AW-16752442655"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'AW-16752442655');
+    </script>
+    {{-- End Google tag --}}
 </head>
 <body>
 <noscript>
